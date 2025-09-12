b44ca90fb8a8a0cae689393783b7efc3ecd0099f
Gerardo Lopez <gerardoglg12@gmail.com>
2025-09-12 17:04:34 -0600

    Docs: we add folders and docs

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         aboutUs: resolve(__dirname, "src/pages/aboutUs.html"),
+        card: resolve(__dirname, "src/pages/card.html"),
       },
     },
   },
