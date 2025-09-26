eaaf958bed7815ae77fcb87d77eb5e3abf150f0d
Ivan <thecubeg@gmail.com>
2025-09-26 17:10:18 -0600

    refact: fixing dinamic nav bar

--- src/pages/aboutUs/aboutUs.js
+++ src/pages/aboutUs/aboutUs.js
@@ -1,24 +1,32 @@
+import { insertDiv } from "/src/common/navBar/navBar.js";
+import "/src/common/navBar/navBar.css";
+
 // Inicializar el carrusel manualmente
-document.addEventListener('DOMContentLoaded', function() {
-    var carousel = new bootstrap.Carousel(document.getElementById('carouselExample'), {
-        wrap: true
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var carousel = new bootstrap.Carousel(
+    document.getElementById("carouselExample"),
+    {
+      wrap: true,
+    }
+  );
 });
-const map = L.map('map').setView([23.634501, -102.552784], 4);
+const map = L.map("map").setView([23.634501, -102.552784], 4);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright"></a>'
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright"></a>',
 }).addTo(map);
-
 
 // Crear un icono personalizado con el número 12
 const numberIcon = L.divIcon({
-    className: 'number-icon',
-    html: '<div>12</div>',
-    iconSize: [30, 30],
-    iconAnchor: [15, 15]
+  className: "number-icon",
+  html: "<div>12</div>",
+  iconSize: [30, 30],
+  iconAnchor: [15, 15],
 });
 
 // Añadir marcador con el número 12 en México (Coordenadas aproximadas de Ciudad de México)
-L.marker([19.4326, -99.1332], {icon: numberIcon})
-    .addTo(map)
+L.marker([19.4326, -99.1332], { icon: numberIcon }).addTo(map);
+
+document.addEventListener("DOMContentLoaded", () => {
+  insertDiv();
+});
---
+++ src/pages/main/main.js
@@ -0,0 +1,8 @@
+import { insertDiv } from "/src/common/navBar/navBar.js";
+import "/src/common/navBar/navBar.css";
+
+console.log("hola amigos");
+
+document.addEventListener("DOMContentLoaded", () => {
+  insertDiv();
+});
