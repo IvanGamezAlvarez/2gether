5a76c3674f9c97cef89f85a1e54ac217f695ef89
Ivan Gamez Alvarez <107066342+IvanGamezAlvarez@users.noreply.github.com>
2025-10-12 15:29:31 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         landing: resolve(__dirname, "src/pages/main/main.html"),
-        aboutUs: resolve(__dirname, "src/pages/aboutUs/aboutUs.html"),
+        aboutUs: resolve(__dirname, "src/pages/aboutUs/aboutus.html"),
         card: resolve(__dirname, "src/pages/card/card.html"),
         register: resolve(__dirname, "src/pages/register/Registro.html"),
         chat: resolve(__dirname, "src/pages/chat/chat.html"),
